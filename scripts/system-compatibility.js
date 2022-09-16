576be4e3c9b823e1e2ad6a8d26a5ca2a094e4ffb
itamar <itamarcu@gmail.com>
2022-09-16 18:28:07 +0300

    Upgraded to V10 compatibility

--- scripts/system-compatibility.js
+++ scripts/system-compatibility.js
@@ -1,28 +1,28 @@
 export const systemBasedHpFromActor = (actor) => {
   if (game.system.id === 'pf2e') {
     return {
-      currentHp: actor.data.data.attributes.hp?.value,
-      maxHp: actor.data.data.attributes.hp?.max,
+      currentHp: actor.system.attributes.hp?.value,
+      maxHp: actor.system.attributes.hp?.max,
     }
   } else if (game.system.id === 'dnd5e') {
     return {
-      currentHp: actor.data.data.attributes.hp.value,
-      maxHp: actor.data.data.attributes.hp.max,
+      currentHp: actor.system.attributes.hp.value,
+      maxHp: actor.system.attributes.hp.max,
     }
   } else if (game.system.id === 'swade') {
     return {
-      currentHp: actor.data.data.wounds.max - actor.data.data.wounds.value,
-      maxHp: actor.data.data.wounds.max,
+      currentHp: actor.system.wounds.max - actor.system.wounds.value,
+      maxHp: actor.system.wounds.max,
     }
   } else if (game.system.id === 'hexxen-1733') {
     return {
-      currentHp: actor.data.data.health.value,
-      maxHp: actor.data.data.health.max,
+      currentHp: actor.system.health.value,
+      maxHp: actor.system.health.max,
     }
   } else if (game.system.id === 'wfrp4e') {
     return {
-      currentHp: actor.data.data.status.wounds.value,
-      maxHp: actor.data.data.status.wounds.max,
+      currentHp: actor.system.status.wounds.value,
+      maxHp: actor.system.status.wounds.max,
     }
   } else {
     // not a supported system
@@ -35,15 +35,15 @@ export const systemBasedHpFromActor = (actor) => {
 export const systemBasedHpFromUpdate = (actor, data) => {
   if (game.system.id === 'pf2e') {
     // hpDiff = options.damageTaken
-    return data.data?.attributes?.hp?.value
+    return data.system?.attributes?.hp?.value
   } else if (game.system.id === 'dnd5e') {
-    return data.data?.attributes?.hp?.value
+    return data.system?.attributes?.hp?.value
   } else if (game.system.id === 'swade') {
-    return actor?.data?.data?.wounds?.max - data.data?.wounds?.value
+    return actor?.system?.wounds?.max - data.system?.wounds?.value
   } else if (game.system.id === 'hexxen-1733') {
-    return data.data?.health?.value
+    return data.system?.health?.value
   } else if (game.system.id === 'wfrp4e') {
-    return data.data?.status?.wounds?.value
+    return data.system?.status?.wounds?.value
   } else {
     // not a supported system
     return undefined
